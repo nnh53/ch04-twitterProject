927bd8f02a5efbc2f67519fb5e6f56301505d236
nnh53 <nnh53.work@gmail.com>
2023-11-09 19:31:04 +0700

    write document

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -2,37 +2,52 @@ import { Request, Response, NextFunction } from 'express'
 import { validationResult, ValidationChain } from 'express-validator'
 import { RunnableValidationChains } from 'express-validator/src/middlewares/schema'
 import { EntityError, ErrorWithStatus } from '~/models/Errors'
-// can be reused by many routes
 
-// sequential processing, stops running validations chain if the previous one fails.
-export const validate = (validation: RunnableValidationChains<ValidationChain>) => {
+type Middleware = {
+  (req: Request, res: Response, next: NextFunction): Promise<void>
+}
+
+/**
+ * This function return a middleware that have handle all the 'validation'.
+ * If the error come from 'validation'(mean have no status), will be assigned status 422 and combine
+ * with other 422 error
+ * @param validation
+ * @returns Middleware
+ */
+export const validate = (validation: RunnableValidationChains<ValidationChain>): Middleware => {
   return async (req: Request, res: Response, next: NextFunction) => {
+    // chạy qua tất cả các ValidationChain trong validation
+    // NẾU TỪNG CHAIN CÓ LỖI THÌ SẼ TRUYỀN VÀO REQ và đi tiếp
     await validation.run(req)
 
-    const errors = validationResult(req)
+    const allErrors = validationResult(req)
 
-    if (errors.isEmpty()) {
+    if (allErrors.isEmpty()) {
       return next()
     }
 
-    const errorObject = errors.mapped()
+    // map lại error của tất cả các chain lại (KỂ CẢ KHÔNG PHẢI TỪ validation chain)
+    const allErrorMappedObject = allErrors.mapped()
+    console.log(allErrorMappedObject)
+
     const entityError = new EntityError({ errors: {} })
 
-    // xử lý errorObject
-    for (const key in errorObject) {
-      // lấy msg từ của từng cái lỗi
-      const { msg } = errorObject[key]
-      // nếu msg có dạng ErrorWithStatus và status !== 422 thì ném về error handler tổng
+    // xử lý những error gửi từ validation chain (KHÔNG CÓ STATUS 422)
+    // duyệt qua từng key của allErrorMappedObject
+    for (const key in allErrorMappedObject) {
+      // vào trong từng key của allErrorMappedObject tìm kiếm và destructuring ra msg
+      const { msg } = allErrorMappedObject[key]
+
+      // nếu msg có dạng ErrorWithStatus và status !== 422 thì ném tiếp về error handler tổng
       if (msg instanceof ErrorWithStatus && msg.status !== 422) {
         return next(msg)
       }
 
-      // lưu các lỗi 422 từ errorObject vào entityError
-      // xuống đây thì m là 422
+      // xuống đây thì chắc chắn lỗi không có status hoặc status === 422
+      // lưu tất cả vào entityError
       entityError.errors[key] = msg
     }
 
-    // ở đây xử lý lỗi luôn chứ ko ném về error handler tổng
     next(entityError)
   }
 }
