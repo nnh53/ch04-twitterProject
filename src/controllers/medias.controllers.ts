27201de4eea00ed9b31123c43de412d1d76f644a
nnh53 <nnh53.work@gmail.com>
2023-11-18 02:41:23 +0700

    fix bug media upload

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,13 +1,17 @@
 import { NextFunction, Request, Response } from 'express'
 import formidable from 'formidable'
 import path from 'path'
+import fs from 'fs'
+import mime from 'mime'
 import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from '~/constants/dir'
 import { USERS_MESSAGES } from '~/constants/message'
-import mediaServices from '~/services/media.services'
-import { handleUploadImage } from '~/utils/file'
+import mediasServices from '~/services/medias.services'
+import HTTP_STATUS from '~/constants/httpStatus'
+
+// IMAGE
 
 export const uploadImageController = async (req: Request, res: Response) => {
-  const url = await mediaServices.uploadImage(req)
+  const url = await mediasServices.uploadImage(req)
 
   return res.json({
     result: url,
@@ -25,8 +29,10 @@ export const serveImageController = async (req: Request, res: Response, next: Ne
   })
 }
 
+// VIDEO
+
 export const uploadVideoController = async (req: Request, res: Response, next: NextFunction) => {
-  const url = await mediaServices.uploadVideo(req) //uploadVideo chưa làm
+  const url = await mediasServices.uploadVideo(req) //uploadVideo chưa làm
   return res.json({
     message: USERS_MESSAGES.UPLOAD_SUCCESS,
     result: url
@@ -34,11 +40,43 @@ export const uploadVideoController = async (req: Request, res: Response, next: N
 }
 
 //khỏi async vì có đợi gì đâu
-export const serveVideoController = (req: Request, res: Response, next: NextFunction) => {
+export const serveVideoStreamController = (req: Request, res: Response, next: NextFunction) => {
   const { filename } = req.params //lấy filename từ param string
-  res.sendFile(path.resolve(UPLOAD_VIDEO_DIR, filename), (error) => {
-    if (error) {
-      return res.status((error as any).status).send('File not found')
-    }
-  })
+
+  const range = req.headers.range //lấy range từ header
+
+  // lấy kích thước tối đa của video
+  const videoPath = path.resolve(UPLOAD_VIDEO_DIR, filename) //lấy đường dẫn của video từ backend của mình
+
+  // console.log(fs.statSync(videoPath)); lấy ra trạng thái của file
+
+  const videoSize = fs.statSync(videoPath).size //lấy size của video
+
+  // nếu không có range thì yêu cầu range
+
+  if (!range) {
+    return res.status(HTTP_STATUS.BAD_REQUEST).send('Requires Range header')
+  }
+
+  const CHUNK_SIZE = 10 ** 6 //1MB
+
+  const start = Number(range.replace(/\D/g, '')) //lấy số từ range
+
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1) //lấy min của start + chunk_size và videoSize - 1
+  //dung lượng thực tế của video
+  const contentLength = end - start + 1 //lấy độ dài của video
+
+  const contentType = mime.getType(videoPath) || 'video/*' //lấy kiểu của video
+
+  // định dạng header
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': contentType
+  }
+
+  res.writeHead(HTTP_STATUS.PARTIAL_CONTENT, headers) //trả về header
+  const videoStream = fs.createReadStream(videoPath, { start, end }) //tạo stream của video
+  videoStream.pipe(res) //pipe videoStream vào res
 }
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,11 +1,12 @@
 import path from 'path'
 import fs from 'fs'
 import { Request } from 'express'
-import formidable, { File } from 'formidable'
+import formidable from 'formidable'
+import { File } from 'formidable'
 import { UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_DIR, UPLOAD_VIDEO_TEMP_DIR } from '~/constants/dir'
 
 export const initFolder = () => {
-  ;[UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_TEMP_DIR].forEach((dir) => {
+  ;[UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_DIR].forEach((dir) => {
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, {
         recursive: true //cho phép tạo folder nested vào nhau
@@ -29,10 +30,11 @@ export const getFileNameFromFile = (filename: string): string => {
 // prettier-ignore
 export const handleUploadImage = async (req: Request): Promise<File[]> => {
   const form = formidable({
-    uploadDir: path.resolve(UPLOAD_IMAGE_TEMP_DIR), //lưu ở đâu
-    maxFiles: 1, //tối đa bao nhiêu
+    uploadDir: path.resolve(UPLOAD_IMAGE_TEMP_DIR), //tức là formidable sẽ LƯU FILE vào đây trước khi xử lý
+    maxFiles: 4, //tối đa bao nhiêu
     keepExtensions: true, //có lấy đuôi mở rộng không .png, .jpg
-    maxFileSize: 300 * 1024, //tối đa bao nhiêu byte, 300kb
+    maxFileSize: 300 * 1024 *4, //tối đa bao nhiêu byte, 300kb
+    
     filter: function ({ name, originalFilename, mimetype }) { //xài option filter để kiểm tra file có phải là image không
       //name: name|key truyền vào của <input name = bla bla>
       //originalFilename: tên file gốc
@@ -40,6 +42,7 @@ export const handleUploadImage = async (req: Request): Promise<File[]> => {
       // console.log(name, originalFilename, mimetype) //log để xem, nhớ comment
 
       const valid = (name === 'image' && Boolean(mimetype?.includes('image/')))
+
 
       //mimetype? nếu là string thì check, k thì thôi
       //ép Boolean luôn, nếu k thì valid sẽ là boolean | undefined
@@ -53,6 +56,8 @@ export const handleUploadImage = async (req: Request): Promise<File[]> => {
       return valid
     }
   })
+
+
     // form.parse về thành promise
     // files là object có dạng giống hình test code cuối cùng
   return new Promise<File[]>((resolve, reject) => {
@@ -78,8 +83,8 @@ export const handleUploadVideo = async (req: Request) => {
   const form = formidable({
     uploadDir: UPLOAD_VIDEO_DIR, //vì video nên mình không đi qua bước xử lý trung gian nên mình sẽ k bỏ video vào temp
     maxFiles: 1, //tối đa bao nhiêu
-    // keepExtensions: true, //có lấy đuôi mở rộng không .png, .jpg "nếu file có dạng asdasd.app.mp4 thì lỗi, nên mình sẽ xử lý riêng
-    maxFileSize: 50 * 1024 * 1024, //tối đa bao nhiêu byte, 50MB
+    // keepExtensions: true, //có lấy đuôi mở rộng không .png, .jpg "nếu file có dạng video_demo.app.mp4 thì lỗi, nên mình sẽ xử lý riêng
+    maxFileSize: 50 * 1024 * 1024 * 50, //tối đa bao nhiêu byte, 50MB
     //xài option filter để kiểm tra file có phải là video không
     filter: function ({ name, originalFilename, mimetype }) {
       const valid = name === 'video' && Boolean(mimetype?.includes('video/'))
@@ -99,16 +104,26 @@ export const handleUploadVideo = async (req: Request) => {
       if (!files.video) {
         return reject(new Error('video is empty'))
       }
+      // vì mình đã tắt keepExtensions nên mình sẽ phải tự xử lý
       //vì k xài keepExtensions nên file sau khi xử lý xong
       // của mình sẽ k có đuôi mở rộng, mình sẽ rename nó để lắp đuôi cho nó
-      const videos = files.video as File[]
+
+      // trong file{originalFilename: string, newFilename: string, filepath: string}
+      // thì bây giờ mình cần lấy đuôi của originalFilename (bởi vì nó còn đuôi) rồi mình sẽ gắn lại vô cái newFilename
+
+      const videos = files.video as File[] //lấy ra danh sách các video đã upload
+
       videos.forEach((video) => {
-        const ext = getExtension(video.originalFilename as string) //lấy đuôi mở rộng của file cũ
+        //lấy đuôi mở rộng của file cũ
+        const ext = getExtension(video.originalFilename as string)
         //filepath là đường dẫn đến tên file mới đã mất đuôi mở rộng do k dùng keepExtensions
-        fs.renameSync(video.filepath, video.filepath + '.' + ext) //rename lại đường dẫn tên file để thêm đuôi
-        video.newFilename = video.newFilename + '.' + ext //newFilename là tên file mới đã mất đuôi mở rộng do k dùng keepExtensions
+
+        // lắp đuôi vào tên mới
+        fs.renameSync(video.filepath, `${video.filepath}.${ext}`) //rename lại đường dẫn tên file để thêm đuôi
+        video.newFilename += '.' + ext //newFilename là tên file mới đã mất đuôi mở rộng do k dùng keepExtensions
         //lưu lại tên file mới để return ra bên ngoài, thì method uploadVideo khỏi cần thêm đuôi nữa
       })
+
       resolve(files.video as File[])
     })
   })
